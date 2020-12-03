51e8bf44d65a415e48c4ce2b8db0820354143774
Alexey Marchuk <alexeymar@mellanox.com>
2020-12-03 13:47:34 +0300

    rdma: Add functions to work with SRQ
    Add 4 new functions to create/delete SRQ, queue and flush
    recv Work Requests.
    
    Change-Id: I55401d22724cf1ce24f2cca588f2bd1384cdbcd0
    Signed-off-by: Alexey Marchuk <alexeymar@mellanox.com>

---
+++ lib/rdma/common.c
@@ -0,0 +1,127 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright (c) Intel Corporation. All rights reserved.
+ *   Copyright (c) 2020 Mellanox Technologies LTD. All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <rdma/rdma_cma.h>
+#include <infiniband/mlx5dv.h>
+
+#include "spdk/stdinc.h"
+#include "spdk/string.h"
+#include "spdk/likely.h"
+
+#include "spdk_internal/rdma.h"
+#include "spdk/log.h"
+
+struct spdk_rdma_srq *
+spdk_rdma_srq_create(struct spdk_rdma_srq_init_attr *init_attr)
+{
+	assert(init_attr);
+	assert(init_attr->pd);
+
+	struct spdk_rdma_srq *rdma_srq = calloc(1, sizeof(*rdma_srq));
+
+	if (!rdma_srq) {
+		SPDK_ERRLOG("Can't allocate memory for SRQ handle\n");
+		return NULL;
+	}
+
+	rdma_srq->srq = ibv_create_srq(init_attr->pd, &init_attr->srq_init_attr);
+	if (!rdma_srq->srq) {
+		SPDK_ERRLOG("Unable to create SRQ, errno %d (%s)\n", errno, spdk_strerror(errno));
+		free(rdma_srq);
+		return NULL;
+	}
+
+	return rdma_srq;
+}
+
+int
+spdk_rdma_srq_destroy(struct spdk_rdma_srq *rdma_srq)
+{
+	int rc;
+
+	if (!rdma_srq) {
+		return 0;
+	}
+
+	assert(rdma_srq->srq);
+
+	if (rdma_srq->recv_wrs.first != NULL) {
+		SPDK_WARNLOG("Destroying RDMA SRQ with queued recv WRs\n");
+	}
+
+	rc = ibv_destroy_srq(rdma_srq->srq);
+	if (rc) {
+		SPDK_ERRLOG("SRQ destroy failed with %d\n", rc);
+	}
+
+	free(rdma_srq);
+
+	return rc;
+}
+
+bool
+spdk_rdma_srq_queue_recv_wrs(struct spdk_rdma_srq *rdma_srq, struct ibv_recv_wr *first)
+{
+	struct ibv_recv_wr *last;
+
+	last = first;
+	while (last->next != NULL) {
+		last = last->next;
+	}
+
+	if (rdma_srq->recv_wrs.first == NULL) {
+		rdma_srq->recv_wrs.first = first;
+		rdma_srq->recv_wrs.last = last;
+		return true;
+	} else {
+		rdma_srq->recv_wrs.last->next = first;
+		rdma_srq->recv_wrs.last = last;
+		return false;
+	}
+}
+
+int
+spdk_rdma_srq_flush_recv_wrs(struct spdk_rdma_srq *rdma_srq, struct ibv_recv_wr **bad_wr)
+{
+	int rc;
+
+	if (spdk_unlikely(rdma_srq->recv_wrs.first == NULL)) {
+		return 0;
+	}
+
+	rc = ibv_post_srq_recv(rdma_srq->srq, rdma_srq->recv_wrs.first, bad_wr);
+
+	rdma_srq->recv_wrs.first = NULL;
+
+	return rc;
+}
