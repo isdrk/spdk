38ad02c27acdc1ed1e661fed7d01f9ec8e15654e
Andrii Holovchenko <andriih@nvidia.com>
2024-05-16 13:16:21 +0300

    ci: fix rpm upload
    Signed-off-by: Andrii Holovchenko <andriih@nvidia.com>

--- .ci/upload.sh
+++ .ci/upload.sh
@@ -229,7 +229,7 @@ elif [[ -f /etc/redhat-release || -f /etc/openEuler-release ]]; then
         upload_rpm_urm
     elif [ $1 == "nexus" ]; then
         repo_name="${repo_name}-yum"
-        yum install python3-requests -y || yum install python36-requests -y
+        sudo yum install python3-requests -y || sudo yum install python36-requests -y
         # Create YUM repository
         ${bd}/actions/nexus.py yum --name ${repo_name} \
             --url ${REPO_URL} --user ${REPO_USER} \
