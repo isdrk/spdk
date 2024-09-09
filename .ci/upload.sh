4dc9b5309cd96431a8917f8d804f3854bd334ee4
Iaroslav Sydoruk <isydoruk@nvidia.com>
2024-09-09 12:52:40 +0300

    ci: fixed repos URL
    Changed URL for liburing1
    Changed webrepo to nbu
    
    Signed-off-by: Iaroslav Sydoruk <isydoruk@nvidia.com>

--- .ci/upload.sh
+++ .ci/upload.sh
@@ -56,75 +56,6 @@ upload_deb_urm() {
     done
 }
 
-upload_deb_nexus() {
-
-    # Import gpg key
-    gpg --import ${GPG_KEY_PATH}
-
-    shopt -s nullglob
-
-    deb_pkgs=(${name}*${VER}-${REV}_{${arch},all}*.{deb,ddeb})
-    for deb_pkg in ${deb_pkgs[@]}; do
-        test -e $deb_pkg
-        echo "INFO: Signing package ${deb_pkg##*/}"
-        sign_deb=$(dpkg-sig -k ${gpg_key_name} -c ${deb_pkg}|tail -n1)
-        if [ $sign_deb == "NOSIG" ]; then 
-            dpkg-sig -k ${gpg_key_name} -s builder ${deb_pkg}
-        fi
-        upload_url="${REPO_URL}/${repo_name}/"
-        echo "INFO: Uploading package ${deb_pkg} to ${upload_url}"
-        ${bd}/actions/nexus.py apt -u ${REPO_URL} -U ${REPO_USER} \
-            -P ${REPO_PASS} -a upload -n ${repo_name} --file ${deb_pkg}
-
-        if test -n "$release_dir" ; then
-            upload_dir="${release_dir}/${repo_name}/${arch}/"
-            mkdir -p $upload_dir
-            cp -f $deb_pkg $upload_dir
-        fi
-    done
-}
-
-upload_rpm_nexus() {
-
-    releasever=$(get_releasever)
-    if [ -z $releasever ]; then
-        echo "[ERROR]: Unsupported distro. Skip uploading.."
-        exit 1
-    fi
-
-    shopt -s nullglob
-
-    rpms_location=(${HOME}/rpmbuild/RPMS/${arch}/${name}-*${VER}-${REV}*.rpm)
-    for rpm_location in ${rpms_location[@]}; do
-        test -f $rpm_location
-        rpm_name="${rpm_location##*/}"
-        ${bd}/actions/nexus.py yum -u ${REPO_URL} -U $REPO_USER \
-            -P $REPO_PASS -a upload -n ${repo_name} --file ${rpm_location} \
-            --upload_path ${releasever}/${arch}/
-
-        if test -n "$release_dir" ; then
-            upload_dir="${release_dir}/${repo_name}/${releasever}/${arch}/"
-            mkdir -p $upload_dir
-            cp -f $rpm_location $upload_dir
-        fi
-    done
-
-    srpms_location=(${HOME}/rpmbuild/SRPMS/${name}-*${VER}-${REV}*.src.rpm)
-    for srpm_location in ${srpms_location[@]}; do
-        test -f $srpm_location
-        srpm_name="${srpm_location##*/}"
-        ${bd}/actions/nexus.py yum -u ${REPO_URL} -U $REPO_USER \
-            -P $REPO_PASS -a upload -n ${repo_name} --file ${srpm_location} \
-            --upload_path ${releasever}/SRPMS/
-
-        if test -n "$release_dir" ; then
-            upload_dir="${release_dir}/${repo_name}/${releasever}/SRPMS/"
-            mkdir -p $upload_dir
-            cp -f $srpm_location $upload_dir
-        fi
-    done
-}
-
 upload_rpm_urm() {
 
     releasever=$(get_releasever)
@@ -208,17 +139,8 @@ if [[ -f /etc/debian_version ]]; then
     gpg_key_name=$(echo ${GPG_KEY_PATH##*/} | cut -d . -f 1)
 
     if [ $1 == "urm" ]; then
-        REPO_URL="https://urm.nvidia.com/artifactory/sw-nbu-swx-ci-debian-local"
+        REPO_URL="${REPO_URL}/sw-nbu-swx-ci-debian-local"
         upload_deb_urm
-    elif [ $1 == "nexus" ]; then
-        repo_name="${repo_name}-${codename}-apt"
-        # Create APT repository
-        ${bd}/actions/nexus.py apt --name ${repo_name} \
-            --url ${REPO_URL} --user ${REPO_USER} \
-            --password ${REPO_PASS} --action create \
-            --distro ${codename} --keypair-file ${GPG_KEY_PATH%.*}.priv \
-            --write-policy allow || true
-        upload_deb_nexus
     else
         echo "Repo not selected"
         exit 1
@@ -229,16 +151,8 @@ elif [[ -f /etc/redhat-release || -f /etc/openEuler-release ]]; then
     arch=$(uname -m)
     
     if [ $1 == "urm" ]; then
-        REPO_URL="https://urm.nvidia.com/artifactory/sw-nbu-swx-ci-rpm-local"
+        REPO_URL="${REPO_URL}/sw-nbu-swx-ci-rpm-local"
         upload_rpm_urm
-    elif [ $1 == "nexus" ]; then
-        repo_name="${repo_name}-yum"
-        # Create YUM repository
-        ${bd}/actions/nexus.py yum --name ${repo_name} \
-            --url ${REPO_URL} --user ${REPO_USER} \
-            --password ${REPO_PASS} --repo-data-depth 2 \
-            --write-policy allow --action create || true
-        upload_rpm_nexus
     else
         echo "Repo not selected"
         exit 1
