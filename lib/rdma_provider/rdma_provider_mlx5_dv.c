spdk-community-ci/spdk-mirror-for-ci

lib/rdma_provider: Allow to set data_transfer cb
Extend qpair init attr with data_transfer callback
to be set to the memory domain.
verbs provider doesn't support this functionality
The data_transfer callback will be used in next
patches to support accel sequence in nvme_rdma

Signed-off-by: Alexey Marchuk <alexeymar@nvidia.com>
Change-Id: Ibc4aa57057e51ed2e0b687132fdb7a2b4368f5fa

lib/rdma_provider/rdma_provider_mlx5_dv.c
@@ -139,6 +139,9 @@ spdk_rdma_provider_qp_create(struct rdma_cm_id *cm_id,
 		spdk_rdma_provider_qp_destroy(&mlx5_qp->common);
 		return NULL;
 	}
+	if (qp_attr->domain_transfer) {
+		spdk_memory_domain_set_data_transfer(mlx5_qp->common.domain, qp_attr->domain_transfer);
+	}
 
 	qp_attr->cap = dv_qp_attr.cap;
 

isdrk/spdk

lib/rdma_provider/rdma_provider_mlx5_dv.c
@@ -129,6 +129,9 @@ spdk_rdma_provider_qp_create(struct rdma_cm_id *cm_id,
 		spdk_rdma_provider_qp_destroy(&dv_qp->common);
 		return NULL;
 	}
+	if (qp_attr->domain_transfer) {
+		spdk_memory_domain_set_data_transfer(dv_qp->common.domain, qp_attr->domain_transfer);
+	}
 
 	return &dv_qp->common;
 }