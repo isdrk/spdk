spdk-community-ci/spdk-mirror-for-ci

bdev/nvme: Change NVME_CTRLR_DEBUGLOG() to _INFOLOG() for ctrlr reset
Ctrlr reset is not in performance critical I/O path. No log gives no
information about progress. Hence, change related NVME_CTRLR_DEBUGLOG()
to NVME_CTRLR_INFOLOG().

Signed-off-by: Shuhei Matsumoto <smatsumoto@nvidia.com>
Change-Id: Iaef1333c811f8f2c141b2b8d748a3d864ab16681

module/bdev/nvme/bdev_nvme.c
@@ -1738,29 +1738,30 @@ bdev_nvme_disconnected_qpair_cb(struct spdk_nvme_qpair *qpair, void *poll_group_
 			/* We are in a full reset sequence. */
 			if (ctrlr_ch->connect_poller != NULL) {
 				/* qpair was failed to connect. Abort the reset sequence. */
-				NVME_CTRLR_DEBUGLOG(nvme_ctrlr,
-						    "qpair %p was failed to connect. abort the reset ctrlr sequence.\n",
-						    qpair);
+				NVME_CTRLR_INFOLOG(nvme_ctrlr,
+						   "qpair %p was failed to connect. abort the reset ctrlr sequence.\n",
+						   qpair);
 				spdk_poller_unregister(&ctrlr_ch->connect_poller);
 				status = -1;
 			} else {
 				/* qpair was completed to disconnect. Just move to the next ctrlr_channel. */
-				NVME_CTRLR_DEBUGLOG(nvme_ctrlr,
-						    "qpair %p was disconnected and freed in a reset ctrlr sequence.\n",
-						    qpair);
+				NVME_CTRLR_INFOLOG(nvme_ctrlr,
+						   "qpair %p was disconnected and freed in a reset ctrlr sequence.\n",
+						   qpair);
 				status = 0;
 			}
 			nvme_ctrlr_for_each_channel_continue(ctrlr_ch->reset_iter, status);
 			ctrlr_ch->reset_iter = NULL;
 		} else {
 			/* qpair was disconnected unexpectedly. Reset controller for recovery. */
-			NVME_CTRLR_NOTICELOG(nvme_ctrlr, "qpair %p was disconnected and freed. reset controller.\n",
-					     qpair);
+			NVME_CTRLR_INFOLOG(nvme_ctrlr, "qpair %p was disconnected and freed. reset controller.\n",
+					   qpair);
 			bdev_nvme_failover_ctrlr(nvme_ctrlr);
 		}
 	} else {
 		/* In this case, ctrlr_channel is already deleted. */
-		NVME_CTRLR_DEBUGLOG(nvme_ctrlr, "qpair %p was disconnected and freed. delete nvme_qpair.\n", qpair);
+		NVME_CTRLR_NOTICELOG(nvme_ctrlr, "qpair %p was disconnected and freed. delete nvme_qpair.\n",
+				     qpair);
 		nvme_qpair_delete(nvme_qpair);
 	}
 }
@@ -2525,7 +2526,7 @@ bdev_nvme_reset_ctrlr(struct nvme_ctrlr *nvme_ctrlr)
 	nvme_ctrlr->dont_retry = true;
 
 	if (nvme_ctrlr->reconnect_is_delayed) {
-		NVME_CTRLR_DEBUGLOG(nvme_ctrlr, "Reconnect is already scheduled.\n");
+		NVME_CTRLR_INFOLOG(nvme_ctrlr, "Reconnect is already scheduled.\n");
 		msg_fn = bdev_nvme_reconnect_ctrlr_now;
 		nvme_ctrlr->reconnect_is_delayed = false;
 	} else {

isdrk/spdk

module/bdev/nvme/bdev_nvme.c
@@ -2001,29 +2001,30 @@ bdev_nvme_disconnected_qpair_cb(struct spdk_nvme_qpair *qpair, void *poll_group_
 			/* We are in a full reset sequence. */
 			if (ctrlr_ch->connect_poller != NULL) {
 				/* qpair was failed to connect. Abort the reset sequence. */
-				NVME_CTRLR_DEBUGLOG(nvme_ctrlr,
-						    "qpair %p was failed to connect. abort the reset ctrlr sequence.\n",
-						    qpair);
+				NVME_CTRLR_INFOLOG(nvme_ctrlr,
+						   "qpair %p was failed to connect. abort the reset ctrlr sequence.\n",
+						   qpair);
 				spdk_poller_unregister(&ctrlr_ch->connect_poller);
 				status = -1;
 			} else {
 				/* qpair was completed to disconnect. Just move to the next ctrlr_channel. */
-				NVME_CTRLR_DEBUGLOG(nvme_ctrlr,
-						    "qpair %p was disconnected and freed in a reset ctrlr sequence.\n",
-						    qpair);
+				NVME_CTRLR_INFOLOG(nvme_ctrlr,
+						   "qpair %p was disconnected and freed in a reset ctrlr sequence.\n",
+						   qpair);
 				status = 0;
 			}
 			nvme_ctrlr_for_each_channel_continue(ctrlr_ch->reset_iter, status);
 			ctrlr_ch->reset_iter = NULL;
 		} else {
 			/* qpair was disconnected unexpectedly. Reset controller for recovery. */
-			NVME_CTRLR_NOTICELOG(nvme_ctrlr, "qpair %p was disconnected and freed. reset controller.\n",
-					     qpair);
+			NVME_CTRLR_INFOLOG(nvme_ctrlr, "qpair %p was disconnected and freed. reset controller.\n",
+					   qpair);
 			bdev_nvme_failover_ctrlr(nvme_ctrlr);
 		}
 	} else {
 		/* In this case, ctrlr_channel is already deleted. */
-		NVME_CTRLR_DEBUGLOG(nvme_ctrlr, "qpair %p was disconnected and freed. delete nvme_qpair.\n", qpair);
+		NVME_CTRLR_INFOLOG(nvme_ctrlr, "qpair %p was disconnected and freed. delete nvme_qpair.\n",
+				   qpair);
 		nvme_qpair_delete(nvme_qpair);
 	}
 }
@@ -2780,7 +2781,7 @@ bdev_nvme_reset_ctrlr(struct nvme_ctrlr *nvme_ctrlr)
 	nvme_ctrlr->dont_retry = true;
 
 	if (nvme_ctrlr->reconnect_is_delayed) {
-		NVME_CTRLR_DEBUGLOG(nvme_ctrlr, "Reconnect is already scheduled.\n");
+		NVME_CTRLR_INFOLOG(nvme_ctrlr, "Reconnect is already scheduled.\n");
 		msg_fn = bdev_nvme_reconnect_ctrlr_now;
 		nvme_ctrlr->reconnect_is_delayed = false;
 	} else {