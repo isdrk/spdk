5568eac6cd5d2dfdeb8e77f81d5b0ef1eee90834
Alexey Marchuk <alexeymar@nvidia.com>
2025-04-09 18:09:59 +0200

    examples/interrupt_tgt: Create threads and pollers
    Extend functionality of the app by creating a thread
    per reactor and busy/timer poller per thread.
    Further patches will add RPCs to pause/resume pollers
    to test poller's state change in interrupt mode.
    The pollers are immediatelly paused to avoid changes
    in test which expects reactor to be idle, that will
    be removed later
    
    Signed-off-by: Alexey Marchuk <alexeymar@nvidia.com>
    Change-Id: Ieb101b09db4aa0b87d67e74fbef8560735e5a46a

--- examples/interrupt_tgt/interrupt_tgt.c
+++ examples/interrupt_tgt/interrupt_tgt.c
@@ -1,6 +1,7 @@
 /*   SPDX-License-Identifier: BSD-3-Clause
  *   Copyright (C) 2020 Intel Corporation.
  *   All rights reserved.
+ *   Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  */
 
 #include "spdk/stdinc.h"
@@ -15,12 +16,37 @@
 
 #include "spdk_internal/event.h"
 
+struct interrupt_tgt_poller {
+	struct spdk_thread *thread;
+	struct spdk_poller *busy;
+	struct spdk_poller *timer;
+	TAILQ_ENTRY(interrupt_tgt_poller) link;
+};
+
+static struct interrupt_tgt_ctx {
+	TAILQ_HEAD(, interrupt_tgt_poller) pollers;
+	int rc;
+} g_tgt_ctx = {
+	.pollers = TAILQ_HEAD_INITIALIZER(g_tgt_ctx.pollers)
+};
+
 struct rpc_reactor_set_interrupt_mode {
 	int32_t lcore;
 	bool disable_interrupt;
 	int rc;
 	struct spdk_thread *rpc_thread;
 	struct spdk_jsonrpc_request *request;
+};
+
+enum interrupt_tgt_op_on_thread {
+	INTERRUPT_TGT_OP_CREATE,
+	INTERRUPT_TGT_OP_DELETE,
+};
+
+struct interrupt_tgt_op_on_thread_ctx {
+	enum interrupt_tgt_op_on_thread op;
+	int rc;
+	struct interrupt_tgt_ctx *tgt_ctx;
 };
 
 static const struct spdk_json_object_decoder rpc_reactor_set_interrupt_mode_decoders[] = {
@@ -138,8 +164,155 @@ interrupt_tgt_parse_arg(int ch, char *arg)
 }
 
 static void
+interrupt_stop_done(void *ctx)
+{
+	struct interrupt_tgt_op_on_thread_ctx *op_ctx = ctx;
+	struct interrupt_tgt_ctx *tgt_ctx = op_ctx->tgt_ctx;
+
+	free(op_ctx);
+	assert(TAILQ_EMPTY(&tgt_ctx->pollers));
+
+	spdk_app_stop(tgt_ctx->rc);
+}
+
+static int
+poller_noop(void *ctx)
+{
+	return SPDK_POLLER_BUSY;
+}
+
+static int
+timer_noop(void *ctx)
+{
+	return SPDK_POLLER_BUSY;
+}
+
+static void
+interrupt_tgt_op_on_thread(void *ctx)
+{
+	struct interrupt_tgt_op_on_thread_ctx *op_ctx = ctx;
+	struct interrupt_tgt_ctx *tgt_ctx = op_ctx->tgt_ctx;
+	struct interrupt_tgt_poller *poller, *tmp;
+	struct spdk_thread *this_thread = spdk_get_thread();
+
+	if (op_ctx->rc) {
+		return;
+	}
+
+	TAILQ_FOREACH_SAFE(poller, &tgt_ctx->pollers, link, tmp) {
+		if (poller->thread != this_thread) {
+			continue;
+		}
+		switch (op_ctx->op) {
+		case INTERRUPT_TGT_OP_CREATE:
+			poller->busy = SPDK_POLLER_REGISTER(poller_noop, poller, 0);
+			if (!poller->busy) {
+				free(poller);
+				op_ctx->rc = -ENOMEM;
+				return;
+			}
+			spdk_poller_pause(poller->busy);
+			poller->timer = SPDK_POLLER_REGISTER(timer_noop, poller, 1000);
+			if (!poller->timer) {
+				spdk_poller_unregister(&poller->busy);
+				op_ctx->rc = -ENOMEM;
+				return;
+			}
+			spdk_poller_pause(poller->timer);
+			break;
+		case INTERRUPT_TGT_OP_DELETE:
+			TAILQ_REMOVE(&tgt_ctx->pollers, poller, link);
+			spdk_poller_unregister(&poller->busy);
+			spdk_poller_unregister(&poller->timer);
+			spdk_thread_exit(poller->thread);
+			free(poller);
+			break;
+		default:
+			abort();
+		}
+		return;
+	}
+}
+
+static void
+interrupt_tgt_stop(int rc)
+{
+	struct interrupt_tgt_op_on_thread_ctx *op_ctx;
+
+	if (rc) {
+		if (g_tgt_ctx.rc == 0) {
+			g_tgt_ctx.rc = rc;
+		} else {
+			SPDK_WARNLOG("prev rc %d, new rc %d\n", g_tgt_ctx.rc, rc);
+		}
+	}
+	op_ctx = calloc(1, sizeof * op_ctx);
+	if (!op_ctx) {
+		SPDK_ERRLOG("can't allocate memory for op context, skip cleanup\n");
+		spdk_app_stop(rc ? rc : -ENOMEM);
+		return;
+	}
+	op_ctx->tgt_ctx = &g_tgt_ctx;
+	op_ctx->op = INTERRUPT_TGT_OP_DELETE;
+
+	spdk_for_each_thread(interrupt_tgt_op_on_thread, op_ctx, interrupt_stop_done);
+}
+
+static void
+interrupt_tgt_shutdown(void)
+{
+	interrupt_tgt_stop(0);
+}
+
+static void
+create_done(void *ctx)
+{
+	struct interrupt_tgt_op_on_thread_ctx *op_ctx = ctx;
+
+	if (op_ctx->rc) {
+		SPDK_ERRLOG("foreach thread failed, stopping app\n");
+		interrupt_tgt_stop(op_ctx->rc);
+	}
+	free(op_ctx);
+}
+
+static void
 interrupt_tgt_started(void *arg1)
 {
+	char thread_name[32];
+	struct spdk_cpuset cpu_set;
+	struct interrupt_tgt_poller *poller;
+	struct interrupt_tgt_op_on_thread_ctx *op_ctx;
+	struct interrupt_tgt_ctx *tgt_ctx = arg1;
+	uint32_t i;
+
+	SPDK_ENV_FOREACH_CORE(i) {
+		poller = calloc(1, sizeof * poller);
+		if (!poller) {
+			interrupt_tgt_stop(-ENOMEM);
+			return;
+		}
+		snprintf(thread_name, 32, "intr_thr_%u", i);
+		spdk_cpuset_zero(&cpu_set);
+		spdk_cpuset_set_cpu(&cpu_set, i, true);
+		poller->thread = spdk_thread_create(thread_name, &cpu_set);
+		if (!poller->thread) {
+			SPDK_ERRLOG("Failed to create SPDK thread, core %u\n", i);
+			free(poller);
+			interrupt_tgt_stop(-ENOMEM);
+			return;
+		}
+		TAILQ_INSERT_TAIL(&tgt_ctx->pollers, poller, link);
+	}
+	op_ctx = calloc(1, sizeof * op_ctx);
+	if (!op_ctx) {
+		interrupt_tgt_stop(-ENOMEM);
+		return;
+	}
+	op_ctx->op = INTERRUPT_TGT_OP_CREATE;
+	op_ctx->tgt_ctx = tgt_ctx;
+
+	spdk_for_each_thread(interrupt_tgt_op_on_thread, op_ctx, create_done);
 }
 
 int
@@ -150,6 +323,7 @@ main(int argc, char *argv[])
 
 	spdk_app_opts_init(&opts, sizeof(opts));
 	opts.name = "interrupt_tgt";
+	opts.shutdown_cb = interrupt_tgt_shutdown;
 
 	if ((rc = spdk_app_parse_args(argc, argv, &opts, "S:E", NULL,
 				      interrupt_tgt_parse_arg, interrupt_tgt_usage)) !=
@@ -158,7 +332,7 @@ main(int argc, char *argv[])
 	}
 
 	/* Blocks until the application is exiting */
-	rc = spdk_app_start(&opts, interrupt_tgt_started, NULL);
+	rc = spdk_app_start(&opts, interrupt_tgt_started, &g_tgt_ctx);
 
 	spdk_app_fini();
 
