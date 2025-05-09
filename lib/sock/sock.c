7ea0fc6933e6d1897aafd31d64aaf64be1efbe45
Jacek Kalwas <jacek.kalwas@nutanix.com>
2025-05-09 21:06:55 +0200

    sock: add utils to async connect
    Change-Id: I6726c8a8b13c751364ff7aa84e37ccb075b79943
    Signed-off-by: Jacek Kalwas <jacek.kalwas@nutanix.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/25906
    Reviewed-by: Aleksey Marchuk <alexeymar@nvidia.com>
    Reviewed-by: Jim Harris <jim.harris@nvidia.com>
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>

--- lib/sock/sock.c
+++ lib/sock/sock.c
@@ -463,9 +463,6 @@ spdk_sock_posix_fd_create(struct addrinfo *res, struct spdk_sock_opts *opts,
 	return fd;
 }
 
-
-/* Returns 0 on success, -1 on failure, 1 to retry with different address if available.
- * If block set to false it can return -EAGAIN to retry later. */
 static int
 sock_posix_fd_connect_poll(int fd, struct spdk_sock_opts *opts, bool block)
 {
@@ -518,7 +515,13 @@ sock_posix_fd_connect_poll(int fd, struct spdk_sock_opts *opts, bool block)
 }
 
 int
-spdk_sock_posix_fd_connect(int fd, struct addrinfo *res, struct spdk_sock_opts *opts)
+spdk_sock_posix_fd_connect_poll_async(int fd)
+{
+	return sock_posix_fd_connect_poll(fd, NULL, false);
+}
+
+static int
+sock_posix_fd_connect(int fd, struct addrinfo *res, struct spdk_sock_opts *opts, bool block)
 {
 	char portnum[PORTNUMLEN];
 	const char *src_addr;
@@ -566,7 +569,11 @@ spdk_sock_posix_fd_connect(int fd, struct addrinfo *res, struct spdk_sock_opts *
 		return 1;
 	}
 
-	rc = sock_posix_fd_connect_poll(fd, opts, true);
+	if (!block) {
+		return 0;
+	}
+
+	rc = sock_posix_fd_connect_poll(fd, opts, block);
 	if (rc) {
 		return rc;
 	}
@@ -576,6 +583,18 @@ spdk_sock_posix_fd_connect(int fd, struct addrinfo *res, struct spdk_sock_opts *
 	}
 
 	return 0;
+}
+
+int
+spdk_sock_posix_fd_connect_async(int fd, struct addrinfo *res, struct spdk_sock_opts *opts)
+{
+	return sock_posix_fd_connect(fd, res, opts, false);
+}
+
+int
+spdk_sock_posix_fd_connect(int fd, struct addrinfo *res, struct spdk_sock_opts *opts)
+{
+	return sock_posix_fd_connect(fd, res, opts, true);
 }
 
 struct spdk_sock *
