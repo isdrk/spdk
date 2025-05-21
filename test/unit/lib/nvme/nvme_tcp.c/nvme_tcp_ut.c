9a25e172ecd1874932fc1ed2f1c4eccebf2f5d9e
Tomasz Zawadzki <tomasz.zawadzki@nutanix.com>
2025-05-21 23:46:52 +0200

    nvme/tcp: renamed TAILQ_ENTRY for needs_poll list
    Following patch adds another list and entry in
    struct nvme_tcp_qpair, so 'link' is too generic.
    
    Renamed to link_poll, to match needs_poll list.
    
    Change-Id: I31ee6b1a81fe8cab388e1d7ef55a0e0f1105ec99
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@nutanix.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/26003
    Reviewed-by: Konrad Sztyber <ksztyber@nvidia.com>
    Reviewed-by: Jim Harris <jim.harris@nvidia.com>
    Community-CI: Broadcom CI <spdk-ci.pdl@broadcom.com>
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>
    Reviewed-by: Shuhei Matsumoto <smatsumoto@nvidia.com>
    Reviewed-by: Jacek Kalwas <jacek.kalwas@nutanix.com>
    Community-CI: Mellanox Build Bot

--- test/unit/lib/nvme/nvme_tcp.c/nvme_tcp_ut.c
+++ test/unit/lib/nvme/nvme_tcp.c/nvme_tcp_ut.c
@@ -1526,12 +1526,12 @@ test_nvme_tcp_ctrlr_disconnect_qpair(void)
 	TAILQ_INIT(&tqpair.outstanding_reqs);
 	STAILQ_INIT(&tqpair.qpair.free_req);
 	tqpair.qpair.active_free_req = &tqpair.qpair.free_req;
-	TAILQ_INSERT_TAIL(&tgroup.needs_poll, &tqpair, link);
+	TAILQ_INSERT_TAIL(&tgroup.needs_poll, &tqpair, link_poll);
 	TAILQ_INSERT_TAIL(&tqpair.send_queue, &pdu, tailq);
 
 	nvme_tcp_ctrlr_disconnect_qpair(&ctrlr, qpair);
 
-	CU_ASSERT(TAILQ_ENTRY_NOT_ENQUEUED(&tqpair, link));
+	CU_ASSERT(TAILQ_ENTRY_NOT_ENQUEUED(&tqpair, link_poll));
 	CU_ASSERT(tqpair.sock == NULL);
 	CU_ASSERT(TAILQ_EMPTY(&tqpair.send_queue) == true);
 
