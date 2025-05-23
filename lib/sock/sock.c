7cec1ea3724a6338af79c246e2331b485de6c545
Jacek Kalwas <jacek.kalwas@nutanix.com>
2025-05-23 17:32:02 +0200

    sock: simplify spdk_sock_[listen|connect] impl_name handling
    Previously, if NULL was provided and the connection or listen operation
    failed the code would continue checking other sock implementations.
    
    This behavior is no longer necessary, as VPP sock module was removed.
    The remaining uring and posix implementations both rely on network
    kernel stack and upcoming XLIO support shares the same addressing.
    
    This legacy behavior complicates the implementation of asynchronous
    connect, particularly when the socket object is returned immediately.
    
    Now, when NULL is provided, the default implementation is selected and
    terminates on failure without iterating over other sock implementations.
    
    Change-Id: I57c68d131fac8951107d82d6b6e34db7ec2bafcc
    Signed-off-by: Jacek Kalwas <jacek.kalwas@nutanix.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/26077
    Community-CI: Mellanox Build Bot
    Reviewed-by: Tomasz Zawadzki <tomasz@tzawadzki.com>
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>
    Reviewed-by: Jim Harris <jim.harris@nvidia.com>

--- lib/sock/sock.c
+++ lib/sock/sock.c
@@ -623,47 +623,52 @@ spdk_sock_connect(const char *ip, int port, struct spdk_sock_opts *opts)
 	}
 
 	STAILQ_FOREACH_FROM(impl, &g_net_impls, link) {
-		if (impl_name && strncmp(impl_name, impl->name, strlen(impl->name) + 1)) {
-			continue;
-		}
-
-		STAILQ_FOREACH(group_impl, &opts->group->group_impls, link) {
-			if (impl == group_impl->net_impl) {
-				break;
-			}
-		}
-
-		if (group_impl == NULL) {
-			SPDK_ERRLOG("Unable to find group impl!\n");
-			return NULL;
-		}
-
-		SPDK_DEBUGLOG(sock, "Creating a client socket using impl %s\n", impl->name);
-		sock_init_opts(&opts_local, opts);
-		if (opts_local.connect_timeout > INT_MAX) {
-			SPDK_ERRLOG("connect_timeout opt cannot exceed INT_MAX\n");
-			return NULL;
-		}
-
-		sock = impl->connect(ip, port, group_impl, &opts_local);
-		if (sock != NULL) {
-			/* Copy the contents, both the two structures are the same ABI version */
-			memcpy(&sock->opts, &opts_local, sizeof(sock->opts));
-			/* Clear out impl_opts to make sure we don't keep reference to a dangling
-			 * pointer */
-			sock->opts.impl_opts = NULL;
-			sock->net_impl = impl;
-			sock->cb_arg = opts->user_ctx;
-			sock->group_impl = group_impl;
-			TAILQ_INSERT_TAIL(&group_impl->socks, sock, link);
-			TAILQ_INIT(&sock->queued_reqs);
-			TAILQ_INIT(&sock->pending_reqs);
-
-			return sock;
+		if (impl_name && strncmp(impl_name, impl->name, strlen(impl->name) + 1) == 0) {
+			break;
 		}
 	}
 
-	return NULL;
+	if (!impl) {
+		SPDK_ERRLOG("Cannot find %s sock implementation\n", impl_name ? impl_name : "any");
+		return NULL;
+	}
+
+	STAILQ_FOREACH(group_impl, &opts->group->group_impls, link) {
+		if (impl == group_impl->net_impl) {
+			break;
+		}
+	}
+
+	if (group_impl == NULL) {
+		SPDK_ERRLOG("Unable to find group impl!\n");
+		return NULL;
+	}
+
+	SPDK_DEBUGLOG(sock, "Creating a client socket using impl %s\n", impl->name);
+	sock_init_opts(&opts_local, opts);
+	if (opts_local.connect_timeout > INT_MAX) {
+		SPDK_ERRLOG("connect_timeout opt cannot exceed INT_MAX\n");
+		return NULL;
+	}
+
+	sock = impl->connect(ip, port, group_impl, &opts_local);
+	if (!sock) {
+		return NULL;
+	}
+
+	/* Copy the contents, both the two structures are the same ABI version */
+	memcpy(&sock->opts, &opts_local, sizeof(sock->opts));
+	/* Clear out impl_opts to make sure we don't keep reference to a dangling
+	 * pointer */
+	sock->opts.impl_opts = NULL;
+	sock->net_impl = impl;
+	sock->cb_arg = opts->user_ctx;
+	sock->group_impl = group_impl;
+	TAILQ_INSERT_TAIL(&group_impl->socks, sock, link);
+	TAILQ_INIT(&sock->queued_reqs);
+	TAILQ_INIT(&sock->pending_reqs);
+
+	return sock;
 }
 
 struct spdk_sock *
@@ -691,42 +696,47 @@ spdk_sock_listen(const char *ip, int port, struct spdk_sock_opts *opts)
 		return NULL;
 	}
 
-	STAILQ_FOREACH(impl, &g_net_impls, link) {
-		if (impl_name && strncmp(impl_name, impl->name, strlen(impl->name) + 1)) {
-			continue;
-		}
-
-		STAILQ_FOREACH(group_impl, &opts->group->group_impls, link) {
-			if (impl == group_impl->net_impl) {
-				break;
-			}
-		}
-
-		if (group_impl == NULL) {
-			SPDK_ERRLOG("Unable to find group impl!\n");
-			return NULL;
-		}
-
-		SPDK_DEBUGLOG(sock, "Creating a listening socket using impl %s\n", impl->name);
-		sock_init_opts(&opts_local, opts);
-		sock = impl->listen(ip, port, group_impl, &opts_local);
-		if (sock != NULL) {
-			/* Copy the contents, both the two structures are the same ABI version */
-			memcpy(&sock->opts, &opts_local, sizeof(sock->opts));
-			/* Clear out impl_opts to make sure we don't keep reference to a dangling
-			 * pointer */
-			sock->opts.impl_opts = NULL;
-			sock->net_impl = impl;
-			sock->cb_arg = opts->user_ctx;
-			sock->group_impl = group_impl;
-			TAILQ_INSERT_TAIL(&group_impl->socks, sock, link);
-			/* Don't need to initialize the request queues for listen
-			 * sockets. */
-			return sock;
+	STAILQ_FOREACH_FROM(impl, &g_net_impls, link) {
+		if (impl_name && strncmp(impl_name, impl->name, strlen(impl->name) + 1) == 0) {
+			break;
 		}
 	}
 
-	return NULL;
+	if (!impl) {
+		SPDK_ERRLOG("Cannot find %s sock implementation\n", impl_name ? impl_name : "any");
+		return NULL;
+	}
+
+	STAILQ_FOREACH(group_impl, &opts->group->group_impls, link) {
+		if (impl == group_impl->net_impl) {
+			break;
+		}
+	}
+
+	if (group_impl == NULL) {
+		SPDK_ERRLOG("Unable to find group impl!\n");
+		return NULL;
+	}
+
+	SPDK_DEBUGLOG(sock, "Creating a listening socket using impl %s\n", impl->name);
+	sock_init_opts(&opts_local, opts);
+	sock = impl->listen(ip, port, group_impl, &opts_local);
+	if (!sock) {
+		return NULL;
+	}
+
+	/* Copy the contents, both the two structures are the same ABI version */
+	memcpy(&sock->opts, &opts_local, sizeof(sock->opts));
+	/* Clear out impl_opts to make sure we don't keep reference to a dangling
+	 * pointer */
+	sock->opts.impl_opts = NULL;
+	sock->net_impl = impl;
+	sock->cb_arg = opts->user_ctx;
+	sock->group_impl = group_impl;
+	TAILQ_INSERT_TAIL(&group_impl->socks, sock, link);
+	/* Don't need to initialize the request queues for listen
+	 * sockets. */
+	return sock;
 }
 
 struct spdk_sock *
