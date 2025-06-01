0897ab59a71035c7d67829ee3cd7396d0d0dbc66
Michal Berger <michal.berger@nutanix.com>
2025-06-01 23:30:38 +0200

    autotest: Change logic around core-collector
    Instead of telling kernel to pipe cores through it, pick them from
    a location saved as a pattern at the very end.
    
    This is done for couple of reasons:
      - containers: when kernel is told to pipe cores to a helper,
    		it executes it from the main namespace, rather than
    		the namespace of the crashing process (i.e.
    		container's namespace). This means that unless our
    		core-collector is set on the host's side + there's
    		extra logic of rerouting cores to the target container,
    		(e.g. https://review.spdk.io/c/spdk/spdk/+/24919)
    		we are not able to catch these cores at all in
    		tests. Using simple core pattern fixes this since
    		in this case kernel will always write core inside
    		the namespace of the crashing process.
      - selinux: current spdk-ci uses fedora imaging with selinux
    		running in enforcing mode. Default set of policies
    		under fedora restricts binaries from running as a
    		kernel helper (i.e. bash which execute our
    		collector) rendering our collector useless.
    
    The disadvantages to this change are minimal and resolve mainly
    around lack of access to procfs entry of the crashing process at
    the time of writing the core - this info was not that useful in
    a first place and it wasn't something that can't be extracted from
    the actual core.
    
    Change-Id: Ib2de14407add1f74450d294ba2ec329d0c6fe6fb
    Signed-off-by: Michal Berger <michal.berger@nutanix.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/26084
    Reviewed-by: Jim Harris <jim.harris@nvidia.com>
    Reviewed-by: Tomasz Zawadzki <tomasz@tzawadzki.com>
    Reviewed-by: Jacek Kalwas <jacek.kalwas@nutanix.com>
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>

--- scripts/core-collector.sh
+++ scripts/core-collector.sh
@@ -3,27 +3,8 @@
 #  Copyright (C) 2020 Intel Corporation
 #  All rights reserved.
 #
-# We don't want to tell kernel to include %e or %E since these
-# can include whitespaces or other funny characters, and working
-# with those on the cmdline would be a nightmare. Use procfs for
-# the remaining pieces we want to gather:
-# |$rootdir/scripts/core-collector.sh %P %s %t $output_dir
 
-rootdir=$(readlink -f "$(dirname "$0")/../")
-
-maps_to_json() {
-	local _maps=("${maps[@]}")
-	local mem_regions=() mem
-
-	mem_regions=("/proc/$core_pid/map_files/"*)
-
-	for mem in "${!mem_regions[@]}"; do
-		_maps[mem]=\"${_maps[mem]}@${mem_regions[mem]##*/}\"
-	done
-
-	local IFS=","
-	echo "${_maps[*]}"
-}
+shopt -s nullglob
 
 core_meta() {
 	jq . <<- CORE
@@ -32,12 +13,9 @@ core_meta() {
 		    "ts": "$core_time",
 		    "size": "$core_size bytes",
 		    "PID": $core_pid,
+		    "TID": $core_thread,
 		    "signal": "$core_sig ($core_sig_name)",
-		    "path": "$exe_path",
-		    "cwd": "$cwd_path",
-		    "statm": "$statm",
-		    "filter": "$(coredump_filter)",
-		    "mapped": [ $(maps_to_json) ]
+		    "path": "$exe_path"
 		  }
 		}
 	CORE
@@ -45,118 +23,82 @@ core_meta() {
 
 bt() { hash gdb && gdb -batch -ex "thread apply all bt full" "$1" "$2" 2>&1; }
 
-stderr() {
-	exec 2> "$core.stderr.txt"
-	set -x
-}
-
-coredump_filter() {
-	local bitmap bit
-	local _filter filter
-
-	bitmap[0]=anon-priv-mappings
-	bitmap[1]=anon-shared-mappings
-	bitmap[2]=file-priv-mappings
-	bitmap[3]=file-shared-mappings
-	bitmap[4]=elf-headers
-	bitmap[5]=priv-hp
-	bitmap[6]=shared-hp
-	bitmap[7]=priv-DAX
-	bitmap[8]=shared-DAX
-
-	_filter=0x$(< "/proc/$core_pid/coredump_filter")
-
-	for bit in "${!bitmap[@]}"; do
-		((_filter & 1 << bit)) || continue
-		filter=${filter:+$filter,}${bitmap[bit]}
-	done
-
-	echo "$filter"
-}
-
-filter_process() {
-	local excl_binaries=() bin
+filter_exe() {
+	local bin exe="$1"
+	local excl_binaries=()
 
 	# rmem_pool testers simulate crashes to test recovery, so ignore them
 	excl_binaries+=("rmem_pool_test")
 	excl_binaries+=("rmem_pool_write_crash_test*")
 	# FIXME: xfs_io segfaults on some fstests, so ignore it until it's fixed
 	excl_binaries+=("xfs_io")
-	# Add more if needed
 
 	for bin in "${excl_binaries[@]}"; do
 		# The below SC is intentional
 		# shellcheck disable=SC2053
-		[[ ${exe_path##*/} == $bin ]] && return 1
+		[[ $exe == $bin ]] && return 1
 	done
-
-	# Did the process sit in our repo?
-	[[ $cwd_path == "$rootdir"* ]] && return 0
-
-	# Did we load our fio plugins?
-	[[ ${maps[*]} == *"$rootdir/build/fio/spdk_nvme"* ]] && return 0
-	[[ ${maps[*]} == *"$rootdir/build/fio/spdk_bdev"* ]] && return 0
-
-	# Do we depend on it?
-	local crit_binaries=()
-
-	crit_binaries+=("nvme")
-	crit_binaries+=("qemu-system*")
-	# Add more if needed
-
-	for bin in "${crit_binaries[@]}"; do
-		# The below SC is intentional
-		# shellcheck disable=SC2053
-		[[ ${exe_path##*/} == $bin ]] && return 0
-	done
-
-	return 1
+	return 0
 }
 
-args+=(core_pid)
-args+=(core_sig)
-args+=(core_ts)
+parse_core() {
+	local core=$1 _core
+	local cores_dir=$2
 
-read -r "${args[@]}" <<< "$*"
+	local core_pid core_thread
+	local core_save
+	local core_sig core_sig_name
+	local core_size
+	local core_time
+	local exe_comm exe_pat
 
-exe_path=$(readlink -f "/proc/$core_pid/exe")
-cwd_path=$(readlink -f "/proc/$core_pid/cwd")
-exe_comm=$(< "/proc/$core_pid/comm")
-statm=$(< "/proc/$core_pid/statm")
-core_time=$(date -d@"$core_ts")
-core_sig_name=$(kill -l "$core_sig")
-mapfile -t maps < <(readlink -f "/proc/$core_pid/map_files/"*)
+	local prefix=(
+		core_sig
+		core_pid
+		core_thread
+		core_time
+	)
 
-# Filter out processes that we don't care about
-filter_process || exit 0
+	# $output_dir/coredumps/%s-%p-%i-%t-%E.core
+	#  |
+	#  v
+	#  11-47378-47378-1748807733-!opt!spdk!build!bin!spdk_tgt.core
+	_core=${core##*/} _core=${_core%.core}
+	# !opt!spdk!build!bin!spdk_tgt
+	exe_path=${_core#*-*-*-*-}
+	# Split 11-47378-47378-1748807733 into respective variables
+	IFS="-" read -r "${prefix[@]}" <<< "${_core%"-$exe_path"}"
+	# /opt/spdk/build/bin/spdk_tgt
+	exe_path=${exe_path//\!/\/}
+	# spdk_tgt
+	exe_comm=${exe_path##*/}
+	# 11 -> SEGV
+	core_sig_name=$(kill -l "$core_sig")
+	# seconds since Epoch to date
+	core_time=$(date -d"@$core_time")
+	# size in bytes
+	core_size=$(wc -c < "$core")
+	# $output_dir/coredumps/spdk_tgt-47378-47378
+	core_save=$cores_dir/$exe_comm-$core_pid-$core_thread
 
-core=$(< "$rootdir/.coredump_path")/${exe_path##*/}_$core_pid.core
-stderr
+	if filter_exe "$exe_comm"; then
+		# Compress it
+		gzip -c "$core" > "$core_save.gz"
+		# Save the binary
+		cp "$exe_path" "$core_save.bin"
+		# Save the backtrace
+		bt "$exe_path" "$core" > "$core_save.bt.txt"
+		# Save the metadata of the core
+		core_meta > "$core_save.json"
+	fi
+	# Nuke the original core
+	rm "$core"
+}
 
-# RLIMIT_CORE is not enforced when core is piped to us. To make
-# sure we won't attempt to overload underlying storage, copy
-# only the reasonable amount of bytes (systemd defaults to 2G
-# so let's follow that).
-rlimit=$((1024 * 1024 * 1024 * 2))
+cores_dir=$1
 
-# Clear path for lz
-rm -f "$core"{,.{bin,bt,gz,json}}
-
-# Slurp the core
-head -c "$rlimit" <&0 > "$core"
-core_size=$(wc -c < "$core")
-
-# Compress it
-gzip -c "$core" > "$core.gz"
-
-# Save the binary
-cp "$exe_path" "$core.bin"
-
-# Save the backtrace
-bt "$exe_path" "$core" > "$core.bt.txt"
-
-# Save the metadata of the core
-core_meta > "$core.json"
-
-# Nuke the original core
-rm "$core"
+cores=("$cores_dir/"*.core)
+((${#cores[@]} > 0)) || exit 0
+for core in "${cores[@]}"; do
+	parse_core "$core" "$cores_dir"
+done
