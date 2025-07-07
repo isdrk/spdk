b9d6a946e038bdde6cf379aec4f4f0ac8509cf47
Allen Zhu <allenzhu@nvidia.com>
2025-07-07 11:02:32 +0300

    nvme/nvme_rdma: Flush send wrs before disconnecting qpair
    Change-Id: Ibfc437815e4a8ca25f96002677c0586e14f5234f
    Signed-off-by: Allen Zhu <allenzhu@nvidia.com>

--- lib/nvme/nvme_rdma.c
+++ lib/nvme/nvme_rdma.c
@@ -2168,6 +2168,8 @@ nvme_rdma_qpair_flush_send_wrs(struct nvme_rdma_qpair *rqpair)
 static int
 nvme_rdma_qpair_disconnected(struct nvme_rdma_qpair *rqpair, int ret)
 {
+	nvme_rdma_qpair_flush_send_wrs(rqpair);
+
 	if (rqpair->num_active_accel_reqs != 0) {
 		SPDK_DEBUGLOG(nvme, "qp %p has %u accel requests\n", rqpair, rqpair->num_active_accel_reqs);
 		goto lingering;
@@ -2188,8 +2190,6 @@ nvme_rdma_qpair_disconnected(struct nvme_rdma_qpair *rqpair, int ret)
 	if (rqpair->rsps == NULL) {
 		goto quiet;
 	}
-
-	nvme_rdma_qpair_flush_send_wrs(rqpair);
 
 	if (rqpair->need_destroy ||
 	    (rqpair->current_num_sends != 0 ||
