85300545c8db926766c8f7745c28876b270b5895
Aleksey Marchuk <alexeymar@nvidia.com>
2025-08-01 09:54:58 +0000

    lib/mlx5: Disable CQ resize
    We can't resize CQ using verbs API - due to CI mistmatch verbs
    implementation copies not all CQEs and we simply lose completions.
    Resize via DEVX doesn't work too - verbs->kernel creates a CQ using
    physical address and DEVX only allows to resize CQ using umem. Mix of
    PA and umem is rejected by FW.
    
    Change-Id: I544d492e2e02848e2750c6bb567daa02ccd01a82
    Signed-off-by: Aleksey Marchuk <alexeymar@nvidia.com>

--- lib/mlx5/mlx5_qp.c
+++ lib/mlx5/mlx5_qp.c
@@ -693,51 +693,10 @@ spdk_mlx5_cq_destroy(struct spdk_mlx5_cq *cq)
 	return 0;
 }
 
-static inline void
-mlx5_cq_update_consumer_index(struct spdk_mlx5_cq *cq)
-{
-	*((uint32_t *)(uintptr_t)cq->hw.dbrec_addr) = htobe32(cq->hw.ci & 0xffffff);
-}
-
 int
 spdk_mlx5_cq_resize(struct spdk_mlx5_cq *cq, int cqe)
 {
-	struct mlx5dv_obj dv_obj;
-	struct mlx5dv_cq mlx5_cq;
-	int rc;
-
-	/*
-	 * The resize operation fails if the number of outstanding CQEs exceeds the new CQ size.
-	 * Since we do not update the consumer index in the poll function, we must update it here
-	 * to let the HW calculate the number of outstanding CQEs correctly.
-	 */
-	mlx5_cq_update_consumer_index(cq);
-
-	rc = ibv_resize_cq(cq->verbs_cq, cqe);
-	if (rc) {
-		SPDK_ERRLOG("ibv_resize_cq failed, rc %d\n", rc);
-		return rc;
-	}
-
-	dv_obj.cq.in = cq->verbs_cq;
-	dv_obj.cq.out = &mlx5_cq;
-
-	rc = mlx5dv_init_obj(&dv_obj, MLX5DV_OBJ_CQ);
-	if (rc) {
-		SPDK_ERRLOG("Failed to init DV CQ, rc %d\n", rc);
-		return rc;
-	}
-
-	cq->hw.cq_addr = (uintptr_t)mlx5_cq.buf;
-	cq->hw.cqe_cnt = mlx5_cq.cqe_cnt;
-	/*
-	 * The HW produces an extra CQE before switching to a new CQ buffer, and the CQ resize function
-	 * copies non-polled CQEs to the new CQ buffer, excluding the extra one. The index in the new
-	 * buffer is incremented to fill the gap made by the dropped CQE.
-	 */
-	cq->hw.ci++;
-
-	return 0;
+	return -ENOTSUP;
 }
 
 int
