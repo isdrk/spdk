21c0e839dc73ac5455756e26cc34f5fbdf961f49
Jacek Kalwas <jacek.kalwas@nutanix.com>
2025-08-28 23:21:28 +0200

    nvme: always use wait for completion with lock
    In flows where nvme_wait_for_completion_timeout is used, the controller
    mutex is already held. Since the mutex is recursive and for simplicity,
    always use the version that takes the lock.
    
    Remove the function that does not take the lock, as it is no longer used.
    
    Change-Id: I7f069ed71c2f698446c85809d29c156c8b03205e
    Signed-off-by: Jacek Kalwas <jacek.kalwas@nutanix.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/26497
    Community-CI: Mellanox Build Bot
    Reviewed-by: Tomasz Zawadzki <tomasz@tzawadzki.com>
    Reviewed-by: Jim Harris <jim.harris@nvidia.com>
    Reviewed-by: Ben Walker <ben@nvidia.com>
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>

--- lib/nvme/nvme_pcie_common.c
+++ lib/nvme/nvme_pcie_common.c
@@ -1157,7 +1157,8 @@ nvme_pcie_ctrlr_delete_io_qpair(struct spdk_nvme_ctrlr *ctrlr, struct spdk_nvme_
 		free(status);
 		goto free;
 	}
-	if (nvme_wait_for_completion_timeout(ctrlr->adminq, status, ctrlr->opts.admin_timeout_ms * 1000)) {
+	if (nvme_wait_for_completion_robust_lock_timeout(ctrlr->adminq, status, &ctrlr->ctrlr_lock,
+			ctrlr->opts.admin_timeout_ms * 1000)) {
 		if (!status->timed_out) {
 			free(status);
 		}
@@ -1179,7 +1180,8 @@ nvme_pcie_ctrlr_delete_io_qpair(struct spdk_nvme_ctrlr *ctrlr, struct spdk_nvme_
 		free(status);
 		goto free;
 	}
-	if (nvme_wait_for_completion_timeout(ctrlr->adminq, status, ctrlr->opts.admin_timeout_ms * 1000)) {
+	if (nvme_wait_for_completion_robust_lock_timeout(ctrlr->adminq, status, &ctrlr->ctrlr_lock,
+			ctrlr->opts.admin_timeout_ms * 1000)) {
 		if (!status->timed_out) {
 			free(status);
 		}
