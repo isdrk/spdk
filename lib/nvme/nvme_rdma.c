3ef8749719139127e55bab996eab6b8d086a789d
Jacek Kalwas <jacek.kalwas@nutanix.com>
2025-09-26 05:55:27 +0200

    nvme: move in_connect_flag to generic qpair type
    It is common between RDMA and TCP transports and, if needed, could be
    used in the generic layer.
    
    Change-Id: I0b0b7550b8d17ff3db2881c004355deffc9a6dc9
    Signed-off-by: Jacek Kalwas <jacek.kalwas@nutanix.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/26803
    Reviewed-by: Ben Walker <ben@nvidia.com>
    Reviewed-by: Tomasz Zawadzki <tomasz@tzawadzki.com>
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>

--- lib/nvme/nvme_rdma.c
+++ lib/nvme/nvme_rdma.c
@@ -193,8 +193,6 @@ struct nvme_rdma_qpair {
 
 	enum nvme_rdma_qpair_state		state;
 
-	bool					in_connect_poll;
-
 	uint8_t					stale_conn_retry_count;
 	bool					need_destroy;
 	bool					connected;
@@ -1390,11 +1388,11 @@ nvme_rdma_ctrlr_connect_qpair_poll(struct spdk_nvme_ctrlr *ctrlr,
 	struct nvme_rdma_qpair *rqpair = nvme_rdma_qpair(qpair);
 	int rc;
 
-	if (rqpair->in_connect_poll) {
+	if (qpair->in_connect_poll) {
 		return -EAGAIN;
 	}
 
-	rqpair->in_connect_poll = true;
+	qpair->in_connect_poll = true;
 
 	switch (rqpair->state) {
 	case NVME_RDMA_QPAIR_STATE_INVALID:
@@ -1416,7 +1414,7 @@ nvme_rdma_ctrlr_connect_qpair_poll(struct spdk_nvme_ctrlr *ctrlr,
 		if (rc == 0) {
 			rc = -EAGAIN;
 		}
-		rqpair->in_connect_poll = false;
+		qpair->in_connect_poll = false;
 
 		return rc;
 
@@ -1468,8 +1466,7 @@ nvme_rdma_ctrlr_connect_qpair_poll(struct spdk_nvme_ctrlr *ctrlr,
 		break;
 	}
 
-	rqpair->in_connect_poll = false;
-
+	qpair->in_connect_poll = false;
 	return rc;
 }
 
