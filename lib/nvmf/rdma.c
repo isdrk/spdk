3b2dbba2d5e57bf5c1a4d54ae540bcca9481fa2a
Allen Zhu <allenzhu@nvidia.com>
2025-10-14 08:29:18 +0300

    nvmf/rdma: Use rdma_req after free when it's duplicated aer
    In _nvmf_rdma_request_free, rdma_req will be freed when it's
    duplicated aer, so should not use such rdma_req anymore after
    call this function.
    
    Change-Id: I2e8401c75ce5ebb6a36bc859e1d6086dc48c53bf
    Signed-off-by: Allen Zhu <allenzhu@nvidia.com>

--- lib/nvmf/rdma.c
+++ lib/nvmf/rdma.c
@@ -2530,6 +2530,7 @@ nvmf_rdma_request_process(struct spdk_nvmf_rdma_transport *rtransport,
 	int				data_posted;
 	uint32_t			num_blocks, num_rdma_reads_available, qdepth;
 	uint8_t				rsp_needed;
+	bool				is_duplicated_aer;
 
 	rqpair = SPDK_CONTAINEROF(rdma_req->req.qpair, struct spdk_nvmf_rdma_qpair, qpair);
 	device = rqpair->device;
@@ -2980,7 +2981,12 @@ nvmf_rdma_request_process(struct spdk_nvmf_rdma_transport *rtransport,
 					  (uintptr_t)rdma_req, (uintptr_t)rqpair, rqpair->qpair.queue_depth);
 
 			rqpair->poller->stat.request_latency += spdk_get_ticks() - rdma_req->receive_tsc;
+			is_duplicated_aer = rdma_req->is_duplicated_aer;
 			_nvmf_rdma_request_free(rdma_req, rtransport);
+			if (spdk_unlikely(is_duplicated_aer)) {
+				/* rdma_req is freed above because it's duplicated aer */
+				return true;
+			}
 			break;
 		case RDMA_REQUEST_NUM_STATES:
 		default:
