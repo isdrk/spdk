7df0a39900c41ca96f1172ed0e785a12d9bdcb51
Aleksey Marchuk <alexeymar@nvidia.com>
2025-12-02 09:39:41 +0000

    nvmf/rdma: Log WC status before processing a request
    When we reap an error completion of send WR which belongs
    to a duplicated AER, the AER request is freed within request_process
    function and after that we log WR type in nvmf_rdma_log_wc_status - as
    result we hit heap-use-after-free problem. To avoid that we should log
    WC status before processing the request. It make code a bit less
    readable but that is the easiest solution for this problem.
    
    Change-Id: I1b54110f0da1047fb28f4d6a56382030621e37d6
    Signed-off-by: Aleksey Marchuk <alexeymar@nvidia.com>

--- lib/nvmf/rdma.c
+++ lib/nvmf/rdma.c
@@ -5401,6 +5401,8 @@ nvmf_rdma_poller_poll(struct spdk_nvmf_rdma_transport *rtransport,
 				count++;
 				assert(wc[i].opcode == IBV_WC_SEND);
 				assert(nvmf_rdma_req_is_completing(rdma_req));
+			} else {
+				nvmf_rdma_log_wc_status(rqpair, &wc[i]);
 			}
 
 			rdma_req->state = RDMA_REQUEST_STATE_COMPLETED;
@@ -5444,6 +5446,8 @@ nvmf_rdma_poller_poll(struct spdk_nvmf_rdma_transport *rtransport,
 					spdk_nvmf_qpair_disconnect(&rqpair->qpair);
 					break;
 				}
+			} else {
+				nvmf_rdma_log_wc_status(rqpair, &wc[i]);
 			}
 
 			rdma_recv->wr.next = NULL;
@@ -5501,7 +5505,7 @@ nvmf_rdma_poller_poll(struct spdk_nvmf_rdma_transport *rtransport,
 					}
 				}
 			} else {
-				rqpair->ibv_in_error_state = true;
+				nvmf_rdma_log_wc_status(rqpair, &wc[i]);
 				/* If the data transfer fails still force the queue into the error state,
 				 * if we were performing an RDMA_READ, we need to force the request into a
 				 * completed state since it wasn't linked to a send. However, in the RDMA_WRITE
@@ -5539,8 +5543,6 @@ nvmf_rdma_poller_poll(struct spdk_nvmf_rdma_transport *rtransport,
 		/* Handle error conditions */
 		if (spdk_unlikely(wc[i].status)) {
 			rqpair->ibv_in_error_state = true;
-			nvmf_rdma_log_wc_status(rqpair, &wc[i]);
-
 			error = true;
 
 			if (spdk_nvmf_qpair_is_active(&rqpair->qpair)) {
