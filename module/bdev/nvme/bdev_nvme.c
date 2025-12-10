bd95f56305d406fcaad683ba171333b1cded2b89
Jacek Kalwas <jacek.kalwas@nutanix.com>
2025-12-11 00:58:20 +0100

    app, lib/nvme, lib/nvmf, bdev/nvme: update oncs bits to nvme 2.2 defs
    Change ONCS bits naming to conform to NVMe 2.2 specification
    definitions. This alignment will allow for easier grepping
    through the code.
    
    Change-Id: I712bf82af6b0d7a7c842b7e278592575096725ba
    Signed-off-by: Jacek Kalwas <jacek.kalwas@nutanix.com>
    Reviewed-on: https://review.spdk.io/c/spdk/spdk/+/27306
    Reviewed-by: Marcin Gałecki <marcin.galecki@dell.com>
    Community-CI: Mellanox Build Bot
    Tested-by: SPDK Automated Test System <spdkbot@gmail.com>
    Reviewed-by: Tomasz Zawadzki <tomasz@tzawadzki.com>
    Reviewed-by: Jim Harris <jim.harris@nvidia.com>

--- module/bdev/nvme/bdev_nvme.c
+++ module/bdev/nvme/bdev_nvme.c
@@ -3965,11 +3965,11 @@ bdev_nvme_io_type_supported(void *ctx, enum spdk_bdev_io_type io_type)
 
 	case SPDK_BDEV_IO_TYPE_UNMAP:
 		cdata = spdk_nvme_ctrlr_get_data(ctrlr);
-		return cdata->oncs.dsm;
+		return cdata->oncs.nvmdsmsv;
 
 	case SPDK_BDEV_IO_TYPE_WRITE_ZEROES:
 		cdata = spdk_nvme_ctrlr_get_data(ctrlr);
-		return cdata->oncs.write_zeroes;
+		return cdata->oncs.nvmwzsv;
 
 	case SPDK_BDEV_IO_TYPE_COMPARE_AND_WRITE:
 		if (spdk_nvme_ctrlr_get_flags(ctrlr) &
@@ -3988,7 +3988,7 @@ bdev_nvme_io_type_supported(void *ctx, enum spdk_bdev_io_type io_type)
 
 	case SPDK_BDEV_IO_TYPE_COPY:
 		cdata = spdk_nvme_ctrlr_get_data(ctrlr);
-		return cdata->oncs.copy;
+		return cdata->oncs.nvmcpys;
 
 	default:
 		return false;
@@ -5134,7 +5134,7 @@ nbdev_create(struct spdk_bdev *disk, const char *base_name,
 		/* Enable if the Volatile Write Cache exists */
 		disk->write_cache = 1;
 	}
-	if (cdata->oncs.write_zeroes) {
+	if (cdata->oncs.nvmwzsv) {
 		disk->max_write_zeroes = UINT16_MAX + 1;
 	}
 	disk->blocklen = spdk_nvme_ns_get_extended_sector_size(ns);
@@ -5205,7 +5205,7 @@ nbdev_create(struct spdk_bdev *disk, const char *base_name,
 		disk->acwu = cdata->acwu + 1; /* 0-based */
 	}
 
-	if (cdata->oncs.copy) {
+	if (cdata->oncs.nvmcpys) {
 		/* For now bdev interface allows only single segment copy */
 		disk->max_copy = nsdata->mssrl;
 	}
