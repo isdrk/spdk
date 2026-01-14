ecf174974252918597a0ac707cbcad36d5d9ae90
Marek Chomnicki <mchomnicki@nvidia.com>
2026-01-14 08:58:40 +0000

    test/nvmf: temporarily disable interrupt.
    Signed-off-by: Marek Chomnicki <mchomnicki@nvidia.com>

--- test/nvmf/nvmf.sh
+++ test/nvmf/nvmf.sh
@@ -22,5 +22,6 @@ run_test "nvmf_host" $rootdir/test/nvmf/nvmf_host.sh --transport=$SPDK_TEST_NVMF
 # Interrupt mode for now is supported only on the target, with the TCP transport and posix or ssl socket implementations.
 if [[ "$SPDK_TEST_NVMF_TRANSPORT" = "tcp" && $SPDK_TEST_URING -eq 0 ]]; then
 	run_test "nvmf_target_core_interrupt_mode" $rootdir/test/nvmf/nvmf_target_core.sh --transport=$SPDK_TEST_NVMF_TRANSPORT --interrupt-mode
-	run_test "nvmf_interrupt" $rootdir/test/nvmf/target/interrupt.sh --transport=$SPDK_TEST_NVMF_TRANSPORT --interrupt-mode
+	# FIXME: Temporarily disabled for tcp transport.
+	#run_test "nvmf_interrupt" $rootdir/test/nvmf/target/interrupt.sh --transport=$SPDK_TEST_NVMF_TRANSPORT --interrupt-mode
 fi
