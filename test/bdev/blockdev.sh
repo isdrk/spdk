1b5d4f00d34261c029eab2efb33e00912801d188
Shuhei Matsumoto <smatsumoto@nvidia.com>
2026-01-22 18:18:58 -0500

    test/bdev: set qos_test_suite runtime from QOS_RUN_TIME
    Use a QOS_RUN_TIME-based bdevperf duration with lower headroom to keep
    the run under the default RPC timeout while still covering sampling
    windows.
    
    Signed-off-by: Shuhei Matsumoto <smatsumoto@nvidia.com>

--- test/bdev/blockdev.sh
+++ test/bdev/blockdev.sh
@@ -440,8 +440,10 @@ function qos_function_test() {
 }
 
 function qos_test_suite() {
+	local qos_perf_time=$((QOS_RUN_TIME * 3 + 10))
+
 	# Run bdevperf with QoS disabled first
-	"$rootdir/build/examples/bdevperf" -z -m 0x2 -q 256 -o 4096 -w randread -t 60 "$env_ctx" &
+	"$rootdir/build/examples/bdevperf" -z -m 0x2 -q 256 -o 4096 -w randread -t $qos_perf_time "$env_ctx" &
 	QOS_PID=$!
 	echo "Process qos testing pid: $QOS_PID"
 	trap 'cleanup; killprocess $QOS_PID; exit 1' SIGINT SIGTERM EXIT
