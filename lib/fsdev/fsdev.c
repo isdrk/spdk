isdrk/spdk

commit 94dce41863ad70bd1b91a4a2c162ffa693e55725

fsdev: struct spdk_fsdev_notify_data removed
Change-Id: Ife6115dadc30e5cf5f76c7c3a6bea88f8db0c213
Signed-off-by: Anton Nayshtut <anayshtut@nvidia.com>

lib/fsdev/fsdev.c
@@ -1271,27 +1271,27 @@ spdk_fsdev_get_notify_max_data_size(const struct spdk_fsdev *fsdev)
 }
 
 static int
-fsdev_notify_get_type(const struct spdk_fsdev_notify_data *notify_data)
+fsdev_notify_get_type(const struct spdk_fuse_notify_request *request)
 {
 	struct fuse_out_header *out;
 
-	out = notify_data->fuse->iovs[0].iov_base;
+	out = request->iovs[0].iov_base;
 	return out->error;
 }
 
 static int
-fsdev_notify(struct spdk_fsdev *fsdev, const struct spdk_fsdev_notify_data *notify_data,
+fsdev_notify(struct spdk_fsdev *fsdev, const struct spdk_fuse_notify_request *request,
 	     bool is_retry, spdk_fsdev_notify_reply_cb_t reply_cb, void *reply_ctx)
 {
 	int type, res = -ENODEV;
 
 	if (fsdev->internal.notify_cb) {
 		assert(spdk_get_thread() == fsdev->internal.notify_thread);
-		fsdev->internal.notify_cb(fsdev, fsdev->internal.notify_ctx, notify_data,
+		fsdev->internal.notify_cb(fsdev, fsdev->internal.notify_ctx, request,
 					  reply_cb, reply_ctx);
 		res = 0;
 		if (!is_retry) {
-			type = fsdev_notify_get_type(notify_data);
+			type = fsdev_notify_get_type(request);
 			__atomic_add_fetch(&fsdev->internal.hist_stat->notify[type].count, 1,
 					   __ATOMIC_RELAXED);
 		}
@@ -1366,17 +1366,13 @@ fsdev_fuse_notify_done(const struct spdk_fsdev_notify_reply_data *reply, void *c
 static int
 fsdev_notify_fuse(struct spdk_fuse_notify_request *req, bool is_retry)
 {
-	struct spdk_fsdev_notify_data notify_data = {
-		.type = SPDK_FSDEV_NOTIFY_FUSE,
-		.fuse = req,
-	};
 	int rc;
 
 	req->internal.status = 0;
 	req->internal.flags.raw = 0;
 
 	req->internal.flags.in_submit_notify = 1;
-	rc =  fsdev_notify(req->fsdev, &notify_data, is_retry, fsdev_fuse_notify_done, req);
+	rc =  fsdev_notify(req->fsdev, req, is_retry, fsdev_fuse_notify_done, req);
 	req->internal.flags.in_submit_notify = 0;
 
 	return rc;
@@ -2279,12 +2275,13 @@ spdk_fsdev_set_delays(struct spdk_fsdev *fsdev, uint64_t submit_us,
 
 int
 spdk_fsdev_encode_notify(struct iovec *iov, int iovcnt,
-			 const struct spdk_fsdev_notify_data *notify_data,
+			 const struct spdk_fuse_notify_request *req,
 			 uint64_t unique_id)
 {
 	struct fuse_out_header *out_hdr;
 	size_t buf_size;
 	int rc = 0;
+	struct fuse_out_header *req_out;
 
 	buf_size = spdk_iov_length(iov, iovcnt);
 	assert(buf_size >= sizeof(struct fuse_out_header));
@@ -2294,29 +2291,16 @@ spdk_fsdev_encode_notify(struct iovec *iov, int iovcnt,
 		return -ENOMEM;
 	}
 
-	if (notify_data) {
-		if (notify_data->type == SPDK_FSDEV_NOTIFY_FUSE) {
-			struct spdk_fuse_notify_request *req = notify_data->fuse;
-			struct fuse_out_header *req_out = req->iovs[0].iov_base;
+	req_out = req->iovs[0].iov_base;
 
-			if (req_out->len > buf_size) {
-				SPDK_ERRLOG("Buffer is too small for notification, buf_size %lu, notify_size %d\n",
-					    buf_size, req_out->len);
-				rc = -ENOMEM;
-			} else {
-				spdk_copy_iovs_to_buf(out_hdr, buf_size, req->iovs, req->iovcnt);
-				out_hdr->unique = unique_id;
-				rc = 0;
-			}
-		} else {
-			SPDK_ERRLOG("Unsupported notify type %d\n", notify_data->type);
-			rc = -EINVAL;
-		}
+	if (req_out->len > buf_size) {
+		SPDK_ERRLOG("Buffer is too small for notification, buf_size %lu, notify_size %d\n",
+			    buf_size, req_out->len);
+		rc = -ENOMEM;
 	} else {
-		/* error and unique set to zero indicate device reset to driver */
-		out_hdr->len = sizeof(*out_hdr);
-		out_hdr->error = 0;
-		out_hdr->unique = 0;
+		spdk_copy_iovs_to_buf(out_hdr, buf_size, req->iovs, req->iovcnt);
+		out_hdr->unique = unique_id;
+		rc = 0;
 	}
 
 	if (rc == 0) {

include/spdk/fsdev.h
@@ -558,15 +558,6 @@ struct spdk_fuse_notify_request {
 	STAILQ_ENTRY(spdk_fuse_notify_request)	stailq;
 };
 
-struct spdk_fsdev_notify_data {
-	/** Notification type */
-	enum spdk_fsdev_notify_type type;
-	union {
-		/** Data for SPDK_FSDEV_NOTIFY_FUSE notification type */
-		struct spdk_fuse_notify_request *fuse;
-	};
-};
-
 struct spdk_fsdev_notify_reply_data {
 	/** Notification handling status */
 	int status;
@@ -588,7 +579,7 @@ typedef void (*spdk_fsdev_notify_reply_cb_t)(
  *
  * \param fsdev Filesystem device that triggered event.
  * \param ctx Context that was passed in spdk_fsdev_enable_notifications().
- * \param notify_data Data for the filesystem device notification.
+ * \param request Data for the filesystem device notification.
  * Data is only valid in the context of this callback.
  * \param reply_cb Optional notification reply callback. If NULL, fsdev doesn't need a reply for this notification.
  * Fsdev should be ready to get the reply callback in the context of notify callback.
@@ -596,7 +587,7 @@ typedef void (*spdk_fsdev_notify_reply_cb_t)(
  */
 typedef void (*spdk_fsdev_notify_cb_t)(struct spdk_fsdev *fsdev,
 				       void *ctx,
-				       const struct spdk_fsdev_notify_data *notify_data,
+				       const struct spdk_fuse_notify_request *request,
 				       spdk_fsdev_notify_reply_cb_t reply_cb,
 				       void *reply_ctx);
 
@@ -632,7 +623,7 @@ int spdk_fsdev_disable_notifications(struct spdk_fsdev_desc *desc);
 /**
  * Get filesystem device maximum notification data size.
  * It indicates the maximum size of varibale sized data in the notification
- * and does not include fixed size fields in spdk_fsdev_notify_data structure.
+ * and does not include fixed size fields in spdk_fuse_notify_request structure.
  * Example of variable sized data is 'name' in FUSE_NOTIFY_INVAL_ENTRY notification.
  *
  * \param fsdev Filesystem device to query.
@@ -1092,14 +1083,13 @@ int spdk_fsdev_set_delays(struct spdk_fsdev *fsdev, uint64_t submit_us,
  *
  * \param iov Output IO vectors array.
  * \param iovcnt Size of the output IO vectors array.
- * \param notify_data Notification data received from fsdev.
- * Pass NULL to encode "empty" notification that is used to indicate device reset.
+ * \param req Notification request received from fsdev.
  * \param unique_id Unique ID of the notification.
  *
  * \return 0 on success, negated errno on failure.
  */
 int spdk_fsdev_encode_notify(struct iovec *iov, int iovcnt,
-			     const struct spdk_fsdev_notify_data *notify_data,
+			     const struct spdk_fuse_notify_request *req,
 			     uint64_t unique_id);
 
 

lib/fuse/fuse.c
@@ -1128,7 +1128,7 @@ fsdev_fuse_notify_thread_fn(void *arg)
 
 static void
 fsdev_fuse_notify_cb(struct spdk_fsdev *fsdev, void *ctx,
-		     const struct spdk_fsdev_notify_data *notify_data,
+		     const struct spdk_fuse_notify_request *request,
 		     spdk_fsdev_notify_reply_cb_t reply_cb, void *reply_ctx)
 {
 	struct spdk_fuse_mount *mount = ctx;
@@ -1137,7 +1137,7 @@ fsdev_fuse_notify_cb(struct spdk_fsdev *fsdev, void *ctx,
 	struct fuse_notify_request *work;
 	int rc;
 
-	rc = spdk_fsdev_encode_notify(&mount->notify_iov, 1, notify_data, 0);
+	rc = spdk_fsdev_encode_notify(&mount->notify_iov, 1, request, 0);
 	if (rc != 0) {
 		SPDK_ERRLOG("%s: failed to encode notification: %s\n", mount->name,
 			    spdk_strerror(-rc));

test/unit/lib/fsdev/fsdev.c/fsdev_ut.c
@@ -732,16 +732,15 @@ static void *ut_notify_reply_ctx = NULL;
 static void
 ut_fsdev_notify_cb(struct spdk_fsdev *fsdev,
 		   void *ctx,
-		   const struct spdk_fsdev_notify_data *notify_data,
+		   const struct spdk_fuse_notify_request *request,
 		   spdk_fsdev_notify_reply_cb_t reply_cb,
 		   void *reply_ctx)
 {
 	ut_call_record_begin(ut_fsdev_notify_cb);
 	ut_call_record_param_ptr(fsdev);
 	ut_call_record_param_ptr(ctx);
-	ut_call_record_param_hash(notify_data->fuse, offsetof(struct spdk_fuse_notify_request, internal));
-	ut_call_record_param_hash(notify_data->fuse->iovs[0].iov_base,
-				  (uint32_t)notify_data->fuse->iovs[0].iov_len);
+	ut_call_record_param_hash(request, offsetof(struct spdk_fuse_notify_request, internal));
+	ut_call_record_param_hash(request->iovs[0].iov_base, (uint32_t)request->iovs[0].iov_len);
 	ut_call_record_end();
 
 	ut_notify_reply_cb = reply_cb;

commit c798b54fc361700efd55a47d0f9ef1ee47eb947a

lib/fsdev: spdk_fsdev_encode_notify fixed
This patch restores spdk_fsdev_encode_notify() functionality when it is
called with NULL request.

The regression was introduced by commit 94dce418.

Change-Id: Ic7a4e3aac16e97546999a660f42fb341f770ce7d
Signed-off-by: Anton Nayshtut <anayshtut@nvidia.com>

lib/fsdev/fsdev.c
@@ -2285,16 +2285,23 @@ spdk_fsdev_encode_notify(struct iovec *iov, int iovcnt,
 		return -ENOMEM;
 	}
 
-	req_out = req->iovs[0].iov_base;
+	if (req) {
+		req_out = req->iovs[0].iov_base;
 
-	if (req_out->len > buf_size) {
-		SPDK_ERRLOG("Buffer is too small for notification, buf_size %lu, notify_size %d\n",
-			    buf_size, req_out->len);
-		rc = -ENOMEM;
+		if (req_out->len > buf_size) {
+			SPDK_ERRLOG("Buffer is too small for notification, buf_size %lu, notify_size %d\n",
+				    buf_size, req_out->len);
+			rc = -ENOMEM;
+		} else {
+			spdk_copy_iovs_to_buf(out_hdr, buf_size, req->iovs, req->iovcnt);
+			out_hdr->unique = unique_id;
+			rc = 0;
+		}
 	} else {
-		spdk_copy_iovs_to_buf(out_hdr, buf_size, req->iovs, req->iovcnt);
-		out_hdr->unique = unique_id;
-		rc = 0;
+		/* error and unique set to zero indicate device reset to driver */
+		out_hdr->len = sizeof(*out_hdr);
+		out_hdr->error = 0;
+		out_hdr->unique = 0;
 	}
 
 	if (rc == 0) {