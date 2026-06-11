13a886e25022f75fa2c3ea10f2d65ff20d4bcad4
Aleksey Marchuk <alexeymar@nvidia.com>
2026-06-11 14:42:15 +0000

    rdma_utils: Rename error injection vars
    A new error injection mode comes, rename existing
    variables for greater clarity
    
    Signed-off-by: Aleksey Marchuk <alexeymar@nvidia.com>

--- lib/rdma_utils/rdma_utils.c
+++ lib/rdma_utils/rdma_utils.c
@@ -56,10 +56,10 @@ static TAILQ_HEAD(, rdma_utils_memory_domain) g_memory_domains = TAILQ_HEAD_INIT
 			g_memory_domains);
 static pthread_mutex_t g_memory_domains_lock = PTHREAD_MUTEX_INITIALIZER;
 
-static bool g_enable_error_injection = false;
-static enum ibv_wc_status g_error_injection_wc_status = IBV_WC_SUCCESS;
-static uint32_t g_error_inject_rate_num = 0;
-static uint32_t g_error_inject_rate_den = 1;
+static bool g_wc_enable_error_injection = false;
+static enum ibv_wc_status g_wc_error_injection_status = IBV_WC_SUCCESS;
+static uint32_t g_wc_error_inject_rate_num = 0;
+static uint32_t g_wc_error_inject_rate_den = 1;
 
 static int
 rdma_utils_mem_notify(void *cb_ctx, struct spdk_mem_map *map,
@@ -599,14 +599,14 @@ spdk_rdma_cm_id_get_numa_id(struct rdma_cm_id *cm_id)
 void
 spdk_rdma_utils_check_wc_error(int num_completions, struct ibv_wc *wc)
 {
-	if (spdk_unlikely(g_enable_error_injection)) {
+	if (spdk_unlikely(g_wc_enable_error_injection)) {
 		for (int i = 0; i < num_completions; i++) {
 			if (spdk_unlikely(wc[i].status != IBV_WC_SUCCESS)) {
 				continue;
 			}
 
-			if (((uint32_t)rand() % g_error_inject_rate_den) < g_error_inject_rate_num) {
-				wc[i].status = g_error_injection_wc_status;
+			if (((uint32_t)rand() % g_wc_error_inject_rate_den) < g_wc_error_inject_rate_num) {
+				wc[i].status = g_wc_error_injection_status;
 			}
 		}
 	}
@@ -622,11 +622,11 @@ spdk_rdma_utils_inject_wc_error(enum ibv_wc_status status, uint32_t err_rate_num
 		return -EINVAL;
 	}
 
-	g_error_inject_rate_num = err_rate_num;
-	g_error_inject_rate_den = err_rate_den;
-	g_error_injection_wc_status = status;
+	g_wc_error_inject_rate_num = err_rate_num;
+	g_wc_error_inject_rate_den = err_rate_den;
+	g_wc_error_injection_status = status;
 	spdk_smp_mb();
-	g_enable_error_injection = true;
+	g_wc_enable_error_injection = true;
 
 	return 0;
 }
@@ -634,9 +634,9 @@ spdk_rdma_utils_inject_wc_error(enum ibv_wc_status status, uint32_t err_rate_num
 void
 spdk_rdma_utils_cancel_wc_error(void)
 {
-	g_enable_error_injection = false;
+	g_wc_enable_error_injection = false;
 	spdk_smp_mb();
-	g_error_inject_rate_num = 0;
-	g_error_inject_rate_den = 1;
-	g_error_injection_wc_status = IBV_WC_SUCCESS;
+	g_wc_error_inject_rate_num = 0;
+	g_wc_error_inject_rate_den = 1;
+	g_wc_error_injection_status = IBV_WC_SUCCESS;
 }
